327ef653cb0d598b05cc859bbbb1026c43bcee82
arif rakhmanov <arif.rakhmanov@gmail.com>
2026-09-05 13:17:55 +0300

    feat: add validation for price in ProductCreate model to ensure it is greater than 0

--- backend/schema.py
+++ backend/schema.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class ProductCreate(BaseModel):
     name: str
-    price: float
+    price: float = Field(gt=0)      #Price should be greater than 0
