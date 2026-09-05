9e3ab8674033e69e69af6bb88bdbf320cff51d09
arif rakhmanov <arif.rakhmanov@gmail.com>
2026-09-05 11:41:19 +0300

    feat: add database models and configuration for SQLAlchemy

---
+++ backend/models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Double, String, Integer
+from database import Base
+
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), index=True)
+    price = Column(Double)
